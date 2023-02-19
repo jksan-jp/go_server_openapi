7e045eaa9697c68f1d31a60ee0ab1db363d25361
jksan <jksan.jp@gmail.com>
2023-02-19 10:24:37 +0900

    impl

--- oapicodegen/api/main.go
+++ oapicodegen/api/main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"oapicodegen/lib"
 	"oapicodegen/petstore"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-type Server struct{}
+type petstoreHandler struct{}
+
+type HelloWorld struct {
+	Message string `json:"message"`
+}
+
+// List all pets
+// (GET /pets)
+func (si petstoreHandler) ListPets(ctx echo.Context, params petstore.ListPetsParams) error {
+	return ctx.JSON(http.StatusOK, HelloWorld{
+		Message: "oapi-codegen",
+	})
+}
+
+// Create a pet
+// (POST /pets)
+func (si petstoreHandler) CreatePets(ctx echo.Context) error {
+	return nil
+}
+
+// (GET /pets/{petID})
+func (si petstoreHandler) GetPetsPetID(ctx echo.Context, petID string) error {
+	return nil
+}
+
+// (PATCH /pets/{petID})
+func (si petstoreHandler) PatchPetsPetID(ctx echo.Context, petID string) error {
+	return nil
+}
 
 func main() {
 	e := echo.New()
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	petstoreHandler := petstoreHandler{}
+	petstore.RegisterHandlers(e, petstoreHandler)
 
 	lib.SAY()
-	fmt.Println(petstore.Pet{})
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
